d4b70fe0f8e4a0db9e2ef56b2e0bba65e37068b9
Nicholas Kincaid <nbkincaid@gmail.com>
2015-10-10 10:36:17 -0400

    add board analysis methods in board class to determine full squares, three in a row? and the corresponding marker, with corresponding specs

--- lib/board.rb
+++ lib/board.rb
@@ -1,10 +1,20 @@
 class Board
 
-  def initialize
-    @squares = Array.new(9)
+  SEQUENCES = [ [0,1,2],
+                [3,4,5],
+                [6,7,8],
+                [0,3,6],
+                [1,4,7],
+                [2,5,8],
+                [0,4,8],
+                [2,4,6]
+              ]
+
+  def initialize(squares = nil)
+    @squares = squares || Array.new(9)
   end
 
-  def show
+  def state
     @squares
   end
 
@@ -13,6 +23,34 @@ class Board
       @squares[location] = marker
     end
   end
+
+  def three_in_a_row?
+    ( [@squares[0], @squares[1], @squares[2]].uniq.length == 1 ||
+      [@squares[3], @squares[4], @squares[5]].uniq.length == 1 ||
+      [@squares[6], @squares[7], @squares[8]].uniq.length == 1 ||
+      [@squares[0], @squares[3], @squares[6]].uniq.length == 1 ||
+      [@squares[1], @squares[4], @squares[7]].uniq.length == 1 ||
+      [@squares[2], @squares[5], @squares[8]].uniq.length == 1 ||
+      [@squares[0], @squares[4], @squares[8]].uniq.length == 1 ||
+      [@squares[2], @squares[4], @squares[6]].uniq.length == 1
+    ) && @squares.uniq != [nil]
+  end
+
+  def three_in_a_row_marker
+    SEQUENCES.each do |seq|
+      sequence_vals = [ @squares[seq[0]], @squares[seq[1]], @squares[seq[2]] ].uniq
+      if sequence_vals.length == 1
+        return sequence_vals[0]
+      end
+    end
+    return nil
+  end
+
+  def squares_full?
+    !@squares.include?(nil)
+  end
+
+  # *********************************************************
 
   def valid_marker?(marker)
     if (marker.is_a? String) && (marker.length == 1)
--- lib/spec/board_spec.rb
+++ lib/spec/board_spec.rb
@@ -7,22 +7,110 @@ describe Board do
   end
 
   describe "#new" do
-    it "returns a new board object" do
+    it "returns a new board object with 0 input parameters" do
       expect(@board).to be_a Board
+    end
+
+    it "returns a new board object with 1 board input array" do
+      expect(@board).to be_a Board
+    end
+
+    it "initializes an array variable for the input board input array" do
+      @board = Board.new(["X","O","X","O","X","O","X","O","X"])
+      expect(@board.state).to eq(["X","O","X","O","X","O","X","O","X"])
     end
   end
 
-  describe "#show" do
+  describe "#state" do
     it "returns an array representating the state of the squares" do
-      expect(@board.show).to eq([nil,nil,nil,nil,nil,nil,nil,nil,nil])
+      expect(@board.state).to eq([nil,nil,nil,nil,nil,nil,nil,nil,nil])
     end
   end
 
   describe "#place_marker" do
     it "should place a specified marker on a specified spot" do
       @board.place_marker(7, "X")
-      expect(@board.show).to eq([nil,nil,nil,nil,nil,nil,nil,"X",nil])
+      expect(@board.state).to eq([nil,nil,nil,nil,nil,nil,nil,"X",nil])
     end
+  end
+
+
+  describe "#three_in_a_row?" do
+    it "should return true if there is a match in the top horizontal row" do
+      board = Board.new(["X","X","X","O","O",nil,nil,nil,nil])
+      expect(board.three_in_a_row?).to eq(true)
+    end
+
+    it "should return true if there is a match in the middle horizontal row" do
+      board = Board.new(["X","X",nil,"O","O","O",nil,"X",nil])
+      expect(board.three_in_a_row?).to eq(true)
+    end
+
+    it "should return true if there is a match in the middle horizontal row" do
+      board = Board.new(["O","O",nil,nil,nil,nil,"X","X","X"])
+      expect(board.three_in_a_row?).to eq(true)
+    end
+
+    it "should return true if there is a match in the left vertical column" do
+      board = Board.new(["O",nil,"X","O",nil,nil,"O",nil,"X"])
+      expect(board.three_in_a_row?).to eq(true)
+    end
+
+    it "should return true if there is a match in the middle vertical column" do
+      board = Board.new(["O","X",nil,nil,"X",nil,"O","X",nil])
+      expect(board.three_in_a_row?).to eq(true)
+    end
+
+    it "should return true if there is a match in the right vertical column" do
+      board = Board.new(["X",nil,"O","X",nil,"O",nil,nil,"O"])
+      expect(board.three_in_a_row?).to eq(true)
+    end
+
+    it "should return true if there is a match in the left diagonal" do
+      board = Board.new(["X",nil,"O",nil,"X","O",nil,"O","X"])
+      expect(board.three_in_a_row?).to eq(true)
+    end
+
+    it "should return true if there is a match in the right diagonal" do
+      board = Board.new(["X","X","O",nil,"O",nil,"O",nil,nil])
+      expect(board.three_in_a_row?).to eq(true)
+    end
+
+    it "should return false if there is no match in an empty board" do
+      board = Board.new([nil,nil,nil,nil,nil,nil,nil,nil,nil])
+      expect(board.three_in_a_row?).to eq(false)
+    end
+
+    it "should return false if there is no match in a full board" do
+      board = Board.new(["X","O","X","O","X","X","O","X","O"])
+      expect(board.three_in_a_row?).to eq(false)
+    end
+  end
+
+
+  describe "#squares_full?" do
+    it "should return true for a count of 0 nil values" do
+      board = Board.new(["X","O","X","O","X","O","X","O","X"])
+      expect(board.squares_full?).to eq(true)
+    end
+
+    it "should return false for more than 0 nil values" do
+      board = Board.new(["X","O","X","O", nil,"O","X","O","X"])
+      expect(board.squares_full?).to eq(false)
+    end
+  end
+
+  describe "#three_in_row_marker" do
+    it "should return the correct marker for a top row three in a row sequence" do
+      board = Board.new(["O",nil,"X","O",nil,nil,"O",nil,"X"])
+      expect(board.three_in_a_row_marker).to eq("O")
+    end
+
+    it "should return the correct marker for a right diagonal three in a row sequence" do
+      board = Board.new(["X","X","O",nil,"O",nil,"O",nil,nil])
+      expect(board.three_in_a_row_marker).to eq("O")
+    end
+
   end
 
   describe "#valid_marker?" do
